a39a7d67dd2cc6db570bb5134f477c5f7b8bc741
Wessel van Zeggeren <wessel.van.zeggeren@gmail.com>
2019-06-03 11:28:28 +0200

    change in the CodeModel

--- src/client/model/CodesModel.java
+++ src/client/model/CodesModel.java
@@ -3,31 +3,36 @@ package client.model;
 import java.io.IOException;
 
 public class CodesModel {
-    public static String getLink(String name){
-        switch (name){
-            case "donaldtrump":
-                return "https://www.youtube.com/watch?v=U1mlCPMYtPk";
-            case "poes":
-                return "https://www.youtube.com/watch?v=wZZ7oFKsKzY";
-            case "mark":
-                return "https://www.youtube.com/watch?v=rbd3kAuAR70";
-            case "rick":
-                return "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-            default:
-                return "null";
+
+    public static String getLink(String name) {
+
+        switch (name) {
+
+            case "donaldtrump": return "https://www.youtube.com/watch?v=U1mlCPMYtPk";
+            case "poes":        return "https://www.youtube.com/watch?v=wZZ7oFKsKzY";
+            case "mark":        return "https://www.youtube.com/watch?v=rbd3kAuAR70";
+            case "rick":        return "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+            default:            return "null";
         }
     }
 
-    public static void cheatCode(String code){
+    public static void cheatCode(String code) {
 
-        switch (code){
+        switch (code) {
 
-            case "/rick":
-                try {
-                Runtime.getRuntime().exec("C:\\Program Files\\Internet Explorer\\iexplore.exe https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-                } catch (IOException e) {
-                     e.printStackTrace();
-                }
+            case "/rick": send("C:\\Program Files\\Internet Explorer\\iexplore.exe https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+        }
+    }
+
+    private static void send(String command) {
+
+        try {
+
+            Runtime.getRuntime().exec(command);
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
         }
     }
 }
